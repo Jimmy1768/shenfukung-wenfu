536b88e25ec65379f6c65f09c7261fa4c97f1e7c
Chu-Lin Chuang <jimmy1768@Chu-Lins-MacBook-Pro.local>
2026-08-11 20:09:59 +0800

    fix: stabilize Expo account flows

--- mobile/app/dummy/adapter.js
+++ mobile/app/dummy/adapter.js
@@ -4,11 +4,17 @@ function createDummyAdapter(repository = createDummyRepository()) {
   return {
     kind: 'dummy',
     network: 'disabled',
+    mode: 'dummy',
     signIn: credentials => Promise.resolve().then(() => repository.signIn(credentials)),
     signUp: input => Promise.resolve().then(() => repository.signUp(input)),
     recoverPassword: input => Promise.resolve().then(() => repository.recoverPassword(input)),
     snapshot: () => repository.snapshot(),
     reset: () => repository.reset(),
+    async restoreSession() { return null; },
+    async clearTenantState() { return repository.snapshot(); },
+    async listDependents() { return repository.snapshot(); },
+    async listRegistrations() { return repository.snapshot(); },
+    async loadCollections() { return repository.snapshot(); },
     updateProfile: input => Promise.resolve().then(() => repository.updateProfile(input)),
     createDependent: input => Promise.resolve().then(() => repository.createDependent(input)),
     updateDependent: (id, input) => Promise.resolve().then(() => repository.updateDependent(id, input)),
@@ -18,7 +24,9 @@ function createDummyAdapter(repository = createDummyRepository()) {
     submitAssistance: input => Promise.resolve().then(() => repository.submitAssistance(input)),
     contactTemple: input => Promise.resolve().then(() => repository.contactTemple(input)),
     requestPrivacy: input => Promise.resolve().then(() => repository.requestPrivacy(input)),
-    closeAccount: input => Promise.resolve().then(() => repository.closeAccount(input))
+    closeAccount: input => Promise.resolve().then(() => repository.closeAccount(input)),
+    preferences: async () => repository.snapshot().preferences || {},
+    updatePreferences: input => Promise.resolve().then(() => repository.updatePreferences(input))
   };
 }
 module.exports = { createDummyAdapter };
